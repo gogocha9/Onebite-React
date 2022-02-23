f39a7e4201c844c1b7de85d82a5e5676bdd53d62
YoungHyun Cha <gogocha9@naver.com>
2022-02-23 17:56:18 +0900

    React 기본 배우는 중

---
+++ React 기본/simplediary/src/App.js
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./diaryEditor";
+import DiaryList from "./diaryList";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(1);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  return (
+    <div className="App">
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onDelete={onDelete} diaryList={data} />
+    </div>
+  );
+};
+
+export default App;
--- React.js 기초/React-Exam1/src/Container.js
+++ React.js 기초/React-Exam1/src/Container.js
@@ -1,9 +1,9 @@
-const Container = ({children}) => {
-    return (
-        <div style={{margin: 20, padding: 20, border: "1px solid gray"}}>
-            {children}
-        </div>
-    );
-}
+const Container = ({ children }) => {
+  return (
+    <div style={{ margin: 20, padding: 20, border: "1px solid gray" }}>
+      {children}
+    </div>
+  );
+};
 
 export default Container;
